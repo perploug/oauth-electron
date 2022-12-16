9690692e2b09264c97a4512ca4f2e8782b0176a1
Per Ploug <perp@spotify.com>
2022-12-16 11:47:49 +0100

    missing extensions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import oauth1 from "./lib/oauth1-login";
-import oauth2 from "./lib/oauth2-login";
+import oauth1 from "./lib/oauth1-login.js";
+import oauth2 from "./lib/oauth2-login.js";
 
 export { oauth1, oauth2 };
